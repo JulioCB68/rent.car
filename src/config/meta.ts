963616345aefa0373848f6605119d0286c9b56f9
Júlio César <juliobento237@gmail.com>
2024-05-22 14:09:53 -0300

    chore: setup structure folder, fonts and meta datas

---
+++ src/config/meta.ts
@@ -0,0 +1,7 @@
+import { Metadata } from 'next'
+
+export const metaData: Metadata = {
+  title: 'Rent.Car',
+  description:
+    'Rent.Car is a rent car app developed with Nextjs, Typescript, TailwindCSS and Shadcn/ui',
+}
